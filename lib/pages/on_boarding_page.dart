2c4a6b1f525fad7420da4d27c2fb4f59c0b31c40
rizki <rizkiadnanfutuh@gmail.com>
2024-09-06 01:06:27 +0700

    feat:create a screen display with scaffolding

---
+++ lib/pages/on_boarding_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:saving_app/style/colors.dart';
+import 'package:saving_app/style/text_style.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 100),
+        color: kBgColor.withOpacity(0.5),
+        child: Column(
+          children: [
+            const SafeArea(
+              child: Image(
+                image: AssetImage('assets/images/ob-money.png'),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              padding: const EdgeInsets.all(26),
+              constraints: const BoxConstraints.expand(
+                height: 300,
+                width: 315,
+              ),
+              decoration: const BoxDecoration(
+                color: kWhite,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome!',
+                    style: kHeading5.copyWith(color: kBlack),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    'welcome to fleet finance, the easy way to improve your finance and help you control expenses and income',
+                    style: kSubtitle2.copyWith(color: kSuvaGray),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 70),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: kBlueRibbon,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 14,
+                        horizontal: 36,
+                      ),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: kButton1.copyWith(color: kWhite),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
