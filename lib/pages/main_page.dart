deb08b499df4575c108c9bd516d9b0fceabee1f4
rizki <rizkiadnanfutuh@gmail.com>
2024-09-06 03:27:22 +0700

    feat:add list to change page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:saving_app/pages/home/home_page.dart';
+import 'package:saving_app/pages/portofolio/portofolio_page.dart';
+import 'package:saving_app/pages/profile/profile_page.dart';
+import 'package:saving_app/pages/support/support_page.dart';
 import 'package:saving_app/style/colors.dart';
+import 'package:saving_app/style/text_style.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
   static const nameRoute = '/main';
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  static List<Widget> pages = [
+    const HomePage(),
+    const PortofolioPage(),
+    const SupportPage(),
+    const ProfilePage(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBgColor,
+      body: pages[_selectedIndex],
       bottomNavigationBar: _customBottomNav(),
     );
   }
@@ -24,41 +48,57 @@ class MainPage extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: kGrey,
-            blurRadius: 20,
+            blurRadius: 10,
           ),
         ],
       ),
-      child: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/home.png',
-              width: 24,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(15),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedLabelStyle: kCaption,
+          unselectedLabelStyle: kCaption,
+          selectedItemColor: kBlueRibbon,
+          unselectedItemColor: kDarkGray,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/home.png',
+                width: 24,
+                color: _selectedIndex == 0 ? kBlueRibbon : kDarkGray,
+              ),
+              label: 'Home',
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/portofolio.png',
-              width: 24,
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/portofolio.png',
+                width: 24,
+                color: _selectedIndex == 1 ? kBlueRibbon : kDarkGray,
+              ),
+              label: 'Portofolio',
             ),
-            label: 'Portofolio',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/support.png',
-              width: 24,
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/support.png',
+                width: 24,
+                color: _selectedIndex == 2 ? kBlueRibbon : kDarkGray,
+              ),
+              label: 'Support',
             ),
-            label: 'Support',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/profile.png',
-              width: 24,
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/profile.png',
+                width: 24,
+                color: _selectedIndex == 3 ? kBlueRibbon : kDarkGray,
+              ),
+              label: 'Profile',
             ),
-            label: 'Profile',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/portofolio/portofolio_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PortofolioPage extends StatelessWidget {
+  const PortofolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Portofolio Page'),
+      ),
+    );
+  }
+}
